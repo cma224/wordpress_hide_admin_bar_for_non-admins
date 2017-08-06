93c56afddb80e3823adc14fcfe47e4fa2bf6be3a
Cristian Abello <cristian.abello@valpo.edu>
2017-08-06 02:22:12 +0000

    added hide-admin-bar.php

---
+++ hide-admin-bar.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Plugin Name: Hide Admin Bar from Non-Admins
+Description: This plugin hides the admin bar from your site for all non-Admin users.
+Text Domain: buddypress
+Domain Path: /languages
+Version: 1.0.0
+Author: Cristian Abello
+Author URI: mailto:cristian.abello@valpo.edu
+License: GNU AGPL
+*/
+
+add_action('after_setup_theme','hide_admin_bar_for_non_admins');
+
+function hide_admin_bar_for_non_admins(){
+    if (!current_user_can('administrator') && !is_admin()) 
+        show_admin_bar(false);
+}
+
+?>
